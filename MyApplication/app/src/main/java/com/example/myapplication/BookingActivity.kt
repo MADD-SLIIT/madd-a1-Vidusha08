9dead1eb8cee4025c068001846cc153ee7c01548
Vidusha08 <vidushapherath@gmail.com>
2024-10-02 01:21:46 +0530

    Update Booking Funcion

--- MyApplication/app/src/main/java/com/example/myapplication/BookingActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/BookingActivity.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.FrameLayout
 import android.app.DatePickerDialog
 import android.widget.DatePicker
 import android.widget.TextView
